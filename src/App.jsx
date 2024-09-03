2ea7b2f590f5c9e8404fede9d5d6c1de27edbe94
Monde Sineke <nsineke@gmail.com>
2024-09-03 14:46:19 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="heading-section">My first website</h1>
+      <p>This is some text in a paragraph.</p>
+      <button className="button">CLick ME</button>
+    </>
+  );
+}
+
+export default App;
